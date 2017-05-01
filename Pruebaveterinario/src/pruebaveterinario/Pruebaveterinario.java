7f78515fd60db824cbc3200d100f7e23fab6337c
Usuario <Usuario@Usuario-PC>
2017-05-01 21:11:36 +0200

    Segunda modificacion

--- Pruebaveterinario/src/pruebaveterinario/Pruebaveterinario.java
+++ Pruebaveterinario/src/pruebaveterinario/Pruebaveterinario.java
@@ -10,12 +10,17 @@ package pruebaveterinario;
  * @author Usuario
  */
 public class Pruebaveterinario {
-
+    
+String nombre;
+int valor;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    public String getNombre(){
+        return nombre;
     }
     
 }
